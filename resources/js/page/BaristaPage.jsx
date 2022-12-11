1b684eb5c24b5c36a0067ce9b9230ff2bee284b0
Денис <pro@MacBook-Air.Dlink>
2022-12-11 19:26:51 +0300

    adding react structure

---
+++ resources/js/page/BaristaPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BaristaPage = () => {
+  return (
+    <div>BaristaPage</div>
+  )
+}
+
+export default BaristaPage
--- resources/views/components/coffeePot/bonuses/searchForm.blade.php
+++
@@ -1,4 +0,0 @@
-<form action="{{ route('coffeePot.searchUser') }}" method="POST" id="searchForm">
-    @csrf
-    <input type="text" name="value" id="search" placeholder="Введите id/phone">
-</form>
