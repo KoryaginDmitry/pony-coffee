b51a5c94398f6ace6692e214f14152fbf23d5c08
DmitryKoryagin <kor.dima97@mail.ru>
2022-12-11 18:10:32 +0300

    new main layouts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,14 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
-    <title>@yield('title')</title>
+    <title>Pony-coffee</title>
 
     @viteReactRefresh
     @vite('resources/js/app.jsx')
 </head>
 <body>
-    @yield('content')
+    <div id="content"></div>
 </body>
 </html>
